3879abd5aa6406db76e5a65f6ccc015f96aad01d
rakhee11 <rakheekumari51@yahoo.com>
2013-09-22 10:53:09 +0530

    Create phoneFunc.m

---
+++ phoneFunc.m
@@ -0,0 +1,15 @@
+#import <Foundation/foundation.h>
+#import "phoneFunc.h"
+
+@implementation PhoneBookFunc
+
+-(id)initWithName:(NSString*)name
+{
+self=[super init]
+if(self)
+{
+bN=[[NSString alloc]initWithString:name];
+Na=[NSMutableArray alloc]init];
+}
+return self;
+}
