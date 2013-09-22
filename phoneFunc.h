e1b9719de1d5f0159053cc81a5f838db1d84975e
rakhee11 <rakheekumari51@yahoo.com>
2013-09-22 10:52:30 +0530

    Create phoneFunc.h

---
+++ phoneFunc.h
@@ -0,0 +1,10 @@
+#import <foundation/foundation.h >
+#import "phone.h"
+@interface PhoneBookFunc:NSObject 
+
+-(id)initWithName:(NSString*)name;
+-(id)initWithName:(NSString*)email;
+-(id)initWithString:(NSString*)number;
+-(id)init;
+-
+@end
